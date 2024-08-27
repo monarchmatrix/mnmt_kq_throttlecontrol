7afd409b1254eff09d09be6070cb29618e3ea130
Monarch Matrix <166127356+monarchmatrix@users.noreply.github.com>
2024-08-28 04:02:41 +0530

    Update client.lua

--- kq_throttlecontrol/client/client.lua
+++ kq_throttlecontrol/client/client.lua
@@ -1,6 +1,6 @@
 local maxRpm = Config.maxRpm
 local maxSpeed = Config.maxSpeed
-local enabled = true
+local enabled = not Config.smoothingonkey
 local allowedClasses = Config.allowedClasses
 
 local function TriggerThrottleControl()
@@ -9,23 +9,18 @@ local function TriggerThrottleControl()
             if enabled then
                 local veh = GetVehiclePedIsIn(PlayerPedId(), false)
                 local sleep = 50
-                
                 if veh and DoesEntityExist(veh) then
                     local vehClass = GetVehicleClass(veh)
-                    
                     if allowedClasses[vehClass] then
                         if math.abs(GetVehicleThrottleOffset(veh)) > 0.3 and GetEntitySpeed(veh) * 3.6 <= maxSpeed then
                             sleep = 1
-
                             local rpm = GetVehicleCurrentRpm(veh)
-
                             if rpm > maxRpm then
                                 SetVehicleCurrentRpm(veh, maxRpm)
                             end
                         end
                     end
                 end
-                
                 Wait(sleep)
             else
                 Wait(100)
@@ -35,11 +30,19 @@ local function TriggerThrottleControl()
 end
 
 RegisterCommand('+throttlecontrol', function()
-    enabled = false
+    if Config.smoothingonkey then
+        enabled = true
+    else
+        enabled = false
+    end
 end, false)
 
 RegisterCommand('-throttlecontrol', function()
-    enabled = true
+    if Config.smoothingonkey then
+        enabled = false
+    else
+        enabled = true
+    end
 end, false)
 
 TriggerThrottleControl()
